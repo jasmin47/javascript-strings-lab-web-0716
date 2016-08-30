0e02c66b396403f3c22eb90648dc84d1c0a1ece4
jasmin47 <alxsanborn@gmail.com>
2016-08-29 23:13:27 -0400

    done

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ var greetSpecialGuest = "" + specialGuest + "!";
 var topic = "space";
 
 var conversation = `${topic}`;
+
+greeting = "Hello, everybody!"
+greeting;
+
+conversation = `Let\'s talk about ${topic}.`
+conversation;
+
+greetSpecialGuest = "Hello, " + specialGuest + "!"
+greetSpecialGuest
